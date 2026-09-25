c5fa789335f5af48ac10774c72d775c0152f60c2
Prajwal Shrestha <prajwalxstha0000@gmail.com>
2026-09-25 10:08:51 +0545

    comment changes

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,7 +1,6 @@
 function Comment({ comment }) {
   return (
     <div className="flex items-start gap-2 mb-2">
-      <h1>hi hello</h1>
       {comment.userProfilePic ? (
         <img
           src={comment.userProfilePic}
